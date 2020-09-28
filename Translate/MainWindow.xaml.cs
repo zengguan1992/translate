e6f835dfde0fbc517bb996141edfcf0ad4242d36
ZGHOME <919892645@qq.com>
2020-09-28 23:01:37 +0800

    build a project of translate

--- Translate/MainWindow.xaml.cs
+++ Translate/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,59 @@ namespace Translate
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_translate_Click(object sender, RoutedEventArgs e)
+        {
+            tr();
+        }
+
+        public void tr()
+        {
+            string from_str;
+            string to_str;
+            Match match;
+            string result = "Text is error or empty";
+            if ((bool)checkBox_cn.IsChecked)
+            {
+                from_str = textBox_from.Text.ToString().Trim();
+                to_str = TranslateHelper.translateToCN(from_str);
+                match = Regex.Match(to_str, "(?<=\"dst\":\").+(?=\")");
+                if (match.Length > 0)
+                {
+                    result = Regex.Unescape(match.Value);
+                }
+                textBox1_to.Text = result;
+
+            }
+            else if ((bool)checkBox_en.IsChecked)
+            {
+                from_str = textBox_from.Text.ToString().Trim();
+                to_str = TranslateHelper.translateToEN(from_str);
+                match = Regex.Match(to_str, "(?<=\"dst\":\").+(?=\")");
+                if (match.Length > 0)
+                {
+                    result = match.Value;
+                }
+                textBox1_to.Text = result;
+            }
+            else
+            {
+                MessageBox.Show("Please check the language.");
+            }
+        }
+
+        private void chk_Click(object sender, RoutedEventArgs e)
+        {
+            var checkBoxes = new[] { checkBox_cn, checkBox_en };
+            var current = (CheckBox)sender;
+            foreach (var checkBox in checkBoxes)
+            {
+                if (checkBox != current)
+                {
+                    checkBox.IsChecked = !current.IsChecked;
+                }
+            }
         }
     }
 }
---
+++ Translate/TranslateHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Web;
+using System.Net;
+using System.IO;
+
+namespace Translate
+{
+    class TranslateHelper
+    {
+        public static string translateToCN(string str)
+        {
+            // 原文
+            string q = str.ToLower();
+            // 源语言
+            string from = "en";
+            // 目标语言
+            string to = "zh";
+            // 改成您的APP ID
+            string appId = "20200928000576001";
+            Random rd = new Random();
+            string salt = rd.Next(100000).ToString();
+            // 改成您的密钥
+            string secretKey = "yK3PByYjYe9pcjmCJ9Bd";
+            string sign = EncryptString(appId + q + salt + secretKey);
+            string url = "http://api.fanyi.baidu.com/api/trans/vip/translate?";
+            url += "q=" + HttpUtility.UrlEncode(q, Encoding.UTF8);
+            url += "&from=" + from;
+            url += "&to=" + to;
+            url += "&appid=" + appId;
+            url += "&salt=" + salt;
+            url += "&sign=" + sign;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+            request.UserAgent = null;
+            request.Timeout = 6000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+            Console.WriteLine(retString);
+            return retString;
+        }
+        public static string translateToEN(string str)
+        {
+            // 原文
+            string q = str;
+            // 源语言
+            string from = "zh";
+            // 目标语言
+            string to = "en";
+            // 改成您的APP ID
+            string appId = "20200928000576001";
+            Random rd = new Random();
+            string salt = rd.Next(100000).ToString();
+            // 改成您的密钥
+            string secretKey = "yK3PByYjYe9pcjmCJ9Bd";
+            string sign = EncryptString(appId + q + salt + secretKey);
+            string url = "http://api.fanyi.baidu.com/api/trans/vip/translate?";
+            url += "q=" + HttpUtility.UrlEncode(q, Encoding.UTF8);
+            url += "&from=" + from;
+            url += "&to=" + to;
+            url += "&appid=" + appId;
+            url += "&salt=" + salt;
+            url += "&sign=" + sign;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+            request.UserAgent = null;
+            request.Timeout = 6000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+            Console.WriteLine(retString);
+            return retString;
+        }
+        // 计算MD5值
+        public static string EncryptString(string str)
+        {
+            MD5 md5 = MD5.Create();
+            // 将字符串转换成字节数组
+            byte[] byteOld = Encoding.UTF8.GetBytes(str);
+            // 调用加密方法
+            byte[] byteNew = md5.ComputeHash(byteOld);
+            // 将加密结果转换为字符串
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in byteNew)
+            {
+                // 将字节转换成16进制表示的字符串，
+                sb.Append(b.ToString("x2"));
+            }
+            // 返回加密的字符串
+            return sb.ToString();
+        }
+    }
+}
+
